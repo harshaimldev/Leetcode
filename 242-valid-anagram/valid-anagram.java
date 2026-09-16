2dd64202ddff0e03efe7552b4c2cd5893df36adc
Harsh Tiwari <150703852+harshaimldev@users.noreply.github.com>
2026-09-16 21:29:54 +0530

    Time: 3 ms (94.04%) | Memory: 44.4 MB (66.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alpha = new int[26];
+        if (s.length()!=t.length()){
+            return false;
+        }
+        for (int i=0; i<s.length(); i++){
+            alpha[s.charAt(i)-'a']+=1;
+        }
+        for (int i=0; i<s.length(); i++){
+            alpha[t.charAt(i)-'a']-=1;
+        }
+        for (int i=0; i<26; i++){
+            if (alpha[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
